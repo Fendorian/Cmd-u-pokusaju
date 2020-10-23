b70ea423c9285bcbeedf12ca7822f2347dc207d1
Fendorian <papic.stefan24@gmail.com>
2020-10-23 18:11:45 +0200

    First commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,24 @@
                     @endif
                     <a href="/posts/create" class='btn btn-primary'>Create Post </a>
                     <h3>Your Blog Posts</h3>
+                    @if(count($posts) >0)
+                    <table class="table table-striped">
+                        <tr>
+                            <td>Title</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        @foreach($posts as $post)
+                        <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                            <td></td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @else 
+                    <p>You have no posts!</p>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,8 +5,15 @@
 @if(count($posts)>1)
     @foreach ($posts as $post)
         <div class="well">
+            <div class="row">
+                <col class="md-4 col-sm-4">
+                <img style="with:100%" src="/storage/cover_images/{{$post->cover_image}}">
+            </div>
+                <col class="md-4 col-sm-8">
                 <h3> <a href="/posts/{{$post->id}}">{{$post->title}}</h3>
-                <small>Written on {{$post->created_at}}</small>
+                    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+            </div>
+      
         </div>
     @endforeach
     {{$posts->links()}}
